e0c1803218680ebfd013e961607032379740300c
DANIIL_PKHAKADZE <s4101606@student.rmit.edu.au>
2026-05-20 02:05:00 +1000

    playing around with bluesky api

--- fetch_data.py
+++ fetch_data.py
@@ -3,10 +3,25 @@ from atproto import Client
 client = Client()
 client.login("devvotel.bsky.social", "hwd3-k373-bh7x-hu3o")
 
-keywords = ["twitter", "elon", "leaving twitter", "twitter refugee", "miss twitter", "deleted twitter", "quit twitter"]
+keywords = ["twitter", "elon", "twitter refugee", "bluesky vs twitter", "twitter is dead", "quit twitter", "miss twitter"]
 counts = {}
-for kw in keywords:
-    search_res = client.app.bsky.feed.search_posts({"q":kw, "limit":100})
-    counts[kw]= len(search_res.posts)
+#for kw in keywords:
+#    search_res = client.app.bsky.feed.search_posts({"q":kw, "limit":100})
+#    counts[kw]= len(search_res.posts)
+#
+#print(counts)
 
-print(counts)
+cursor = None
+for kw in keywords:
+    count = 0
+    while count <= 2000:
+        if cursor:
+            queryParams = {"q":kw, "limit":100, "lang":"en", "cursor":cursor}
+        else:
+            queryParams = {"q":kw, "limit":100, "lang":"en"}
+        res = client.app.bsky.feed.search_posts(queryParams)
+        count += len(res.posts)
+        cursor = res.cursor
+        if not cursor:
+            break
+        
